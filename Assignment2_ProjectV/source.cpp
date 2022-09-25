6ec3a70b3201caa42158aef7a750620ab6d45827
vdhye <vdhyey02@gmail.com>
2022-09-25 17:30:20 -0400

    Student listed in Vector space commit

--- Assignment2_ProjectV/source.cpp
+++ Assignment2_ProjectV/source.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
+struct STUDENT_DATA {
+	string firstName;
+	string lastName;
+};
+
+void readFile(vector<STUDENT_DATA>& StudentDataVectorObject) {
+
+	ifstream readFile("StudentData.txt");
+
+	if (!readFile.eof()) {
+		string readline;
+		while (getline(readFile, readline))
+		{
+			STUDENT_DATA students;
+			string data;
+			istringstream readData(readline);
+
+			getline(readData, data, ',');
+			students.lastName = data;
+
+			getline(readData, data, '\n');
+			students.firstName = data;
+
+			StudentDataVectorObject.push_back(students);
+		}
+	}
+}
+
 int main() {
+
+	vector<STUDENT_DATA> studentDataVector;
+
+	readFile(studentDataVector);
+
 	return 1;
 }
