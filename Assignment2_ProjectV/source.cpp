b2ca05e19345bdf1c57bca377d0237c3bc842e0a
vdhye <vdhyey02@gmail.com>
2022-09-25 17:35:39 -0400

    Pre release commit step 5

--- Assignment2_ProjectV/source.cpp
+++ Assignment2_ProjectV/source.cpp
@@ -6,14 +6,23 @@
 
 using namespace std;
 
+#define PRE_RELEASE
+
 struct STUDENT_DATA {
 	string firstName;
 	string lastName;
+#ifdef PRE_RELEASE
+	string email;
+#endif
 };
 
 void readFile(vector<STUDENT_DATA>& StudentDataVectorObject) {
 
+#ifdef PRE_RELEASE
+	ifstream readFile("StudentData_Emails.txt");
+#else
 	ifstream readFile("StudentData.txt");
+#endif
 
 	if (!readFile.eof()) {
 		string readline;
@@ -26,9 +35,13 @@ void readFile(vector<STUDENT_DATA>& StudentDataVectorObject) {
 			getline(readData, data, ',');
 			students.lastName = data;
 
-			getline(readData, data, '\n');
+			getline(readData, data, ',');
 			students.firstName = data;
 
+#ifdef PRE_RELEASE
+			getline(readData, data, '\n');
+			students.email = data;
+#endif
 			StudentDataVectorObject.push_back(students);
 		}
 	}
@@ -36,9 +49,19 @@ void readFile(vector<STUDENT_DATA>& StudentDataVectorObject) {
 
 int main() {
 
+#ifdef PRE_RELEASE 
+	cout << "Pre Release Version" << endl;
+#else 
+	cout << "Standard Version" << endl;
+#endif
+
 	vector<STUDENT_DATA> studentDataVector;
 
+#ifdef PRE_RELEASE
 	readFile(studentDataVector);
+#else
+	readFile(studentDataVector);
+#endif
 
 #ifdef NDEBUG
 	cout << "Select Debug to print names";
@@ -46,7 +69,14 @@ int main() {
 	for (STUDENT_DATA studentDataObject : studentDataVector) {
 		cout << studentDataObject.firstName;
 		cout << " ";
-		cout << studentDataObject.lastName << endl;
+		cout << studentDataObject.lastName;
+#ifdef PRE_RELEASE
+		cout << " ";
+		cout << studentDataObject.email;
+
+#endif				
+		cout << "\n";
 	}
 #endif
+
 }
